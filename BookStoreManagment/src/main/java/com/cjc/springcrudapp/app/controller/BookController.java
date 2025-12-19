545cb423452dab022bf99a4f88337e3e1ed77ca8
hp <hp@192.168.1.49>
2025-12-19 10:13:22 +0530

    add

---
+++ BookStoreManagment/src/main/java/com/cjc/springcrudapp/app/controller/BookController.java
@@ -0,0 +1,5 @@
+package com.cjc.springcrudapp.app.controller;
+
+public class BookController {
+
+}
---
+++ BookStoreManagment/src/main/java/com/cjc/springcrudapp/app/model/Book.java
@@ -0,0 +1,41 @@
+package com.cjc.springcrudapp.app.model;
+
+public class Book {
+	
+	private int bid;
+	private String bname;
+	private int price;
+	private String author;
+	private String type;
+	public int getBid() {
+		return bid;
+	}
+	public void setBid(int bid) {
+		this.bid = bid;
+	}
+	public String getBname() {
+		return bname;
+	}
+	public void setBname(String bname) {
+		this.bname = bname;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
